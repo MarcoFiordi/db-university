adb6d4e880b4b6cfd60cc0f752271bf2f588048a
Marco Fiordi <marco.fiordi97@gmail.com>
2026-06-04 18:10:15 +0200

    inseriti i 4 punti restanti

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -16,5 +16,28 @@ WHERE `students`.`date_of_birth` < '1996-01-01';
 /*Seleziona tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286) */
 SELECT `courses`.*
 FROM `courses`
-WHERE `courses`.`period` = 'I semestre' AND `courses`.`year` = '1';
+WHERE `courses`.`period` = 'I semestre' 
+	AND `courses`.`year` = '1';
+
+/*Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+20/06/2020 (21)
+*/
+SELECT `exams`.*
+FROM `exams`
+WHERE `exams`.`hour` > '14:00:00' 
+	AND `exams`.`date` = '2020-06-20';
+
+/*Selezionare tutti i corsi di laurea magistrale*/
+SELECT `degrees`.*
+FROM `degrees`
+WHERE `degrees`.`level` = 'magistrale';
+
+/*Selezionare tutti i corsi di laurea magistrale*/
+SELECT COUNT(*)
+FROM `departments`;
+
+/*Quanti sono gli insegnanti che non hanno un numero di telefono*/
+SELECT COUNT(*)
+FROM `teachers`
+WHERE `teachers`.`phone`IS NULL;
 
