bd9461389dff8ae1e06a716237c45e2a7a32eb13
Marco Fiordi <marco.fiordi97@gmail.com>
2026-06-05 17:27:00 +0200

    query con join 2-3

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -49,6 +49,23 @@ FROM `students`
     ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = 'corso di laurea in Economia';
 
+/*Selezionare tutti i Corsi di Laurea Magistrale del dipartimento di Neuroscienze*/
+SELECT `degrees`.*
+FROM `degrees`
+	JOIN `departments`
+    ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.`level` = 'magistrale' 
+AND `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+/*Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)*/
+SELECT `courses`.*
+FROM `courses`
+	JOIN `course_teacher`
+	ON `courses`.`id` = `course_teacher`.`course_id`
+		JOIN `teachers`
+        ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `teachers`.`id` = 44;
+
 
 
 
